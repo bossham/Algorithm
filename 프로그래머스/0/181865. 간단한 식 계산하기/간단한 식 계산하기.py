77f864843e2367c7912ec3af0c9bc3bc7092b00c
Jihyun Jung <swp313@gmail.com>
2024-07-05 18:57:26 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,2 +1,12 @@
 def solution(binomial):
-    return eval(binomial)
+    bino = binomial.split()
+    a = int(bino[0])
+    op = bino[1]
+    b = int(bino[2])
+    
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    else:
+        return a * b
