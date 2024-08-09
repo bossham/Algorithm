2d6500309bbc451f11fa8182c7b2d80b0dfbed8b
Jihyun Jung <swp313@gmail.com>
2024-08-10 01:08:03 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,3 +1,7 @@
-a = int(input())
-b = int(input())
-print(a*(b%10), a*((b//10)%10), a*(b//100), a*b, sep='\n')
+a= int(input())
+b= int(input())
+
+print(a * (b % 10))
+print(a * ((b // 10) % 10))
+print(a * (b // 100))
+print(a * b)
