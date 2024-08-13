0495124aede4cc590e62f62736455de148a6bd7b
Jihyun Jung <swp313@gmail.com>
2024-08-13 21:29:09 +0900

    [Bronze IV] Title: 모음의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,14 @@
+vowels = set('aeiouAEIOU')
+
+while True:
+    line = input().strip()
+    
+    if line == '#':
+        break
+        
+    count = 0
+    for char in line:
+        if char in vowels:
+            count += 1
+            
+    print(count)
