1a2def076c651b6a5c8f0c94857bfa3d26421d9e
Jihyun Jung <swp313@gmail.com>
2024-08-25 14:45:47 +0900

    [Bronze III] Title: 공 넣기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,10 +1,8 @@
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+baskets = [0]*n
 
-baskets = [0] * N
-
-for _ in range(M):
+for _ in range(m):
     i, j, k = map(int, input().split())
-    for index in range(i-1, j):
-        baskets[index] = k
-
+    for index in range(i, j+1):
+        baskets[index-1] = k
 print(' '.join(map(str, baskets)))
